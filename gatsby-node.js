7976b39cbaf94237a37cee34820f4a11991cc925
tfiander <travis.fiander@gyroscopic.ca>
2022-01-07 16:36:54 -0500

    configured gastby-node API's to dynamically create pages

---
+++ gatsby-node.js
@@ -0,0 +1,50 @@
+const { create } = require('domain');
+const path = require('path');
+
+module.exports.onCreateNode = ({node, actions}) => {
+    const { createNodeField } = actions;
+
+    if (node.internal.type === "MarkdownRemark") {
+        const slug = path.basename(node.fileAbsolutePath, '.md');
+
+        createNodeField({
+            node,
+            name: "slug",
+            value: slug
+        })
+    }
+}
+
+// this is used to dynamically create new pages for each new post
+ module.exports.createPages = async ({ graphql, actions }) => {
+    const { createPage } = actions;
+
+    // 1. Get path to template
+    const blogTemplate = path.resolve('./src/templates/blog.js');
+
+    // 2. Get markdown data
+    const res = await graphql(`
+        query {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        fields {
+                            slug
+                        }
+                    }
+                }
+            }
+        }
+    `)
+
+    // 3. Create new pages
+    res.data.allMarkdownRemark.edges.forEach((edge) => {
+        createPage({
+            component: blogTemplate,
+            path: `./blog/${edge.node.fields.slug}`,
+            context: {
+                slug: edge.node.fields.slug
+            }
+        })
+    })
+ }
---
+++ src/templates/blog.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import Layout from "../components/Layout";
+
+const Blog = () => {
+    return (
+        <Layout>
+            Test
+        </Layout>
+    )
+}
+
+export default Blog;
